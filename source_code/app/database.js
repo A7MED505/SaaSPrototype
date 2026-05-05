8ba308aa8fffc00056e47aa87fd31a9d2a72e735
AHMED-TAWFIK-HAMID <ahmedahmed20@istanbul.edu.tr>
2026-05-05 19:33:50 +0300

    Fix MongoDB connection: use direct single-host URL

--- source_code/app/database.js
+++ source_code/app/database.js
@@ -5,7 +5,8 @@ const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
 const DATABASE_NAME = process.env.DATABASE_NAME || 'firstsaas';
 
 async function connectDB() {
-  await mongoose.connect(`${MONGO_URL}${DATABASE_NAME}`);
+  const url = `${MONGO_URL}${DATABASE_NAME}?ssl=true&authSource=admin`;
+  await mongoose.connect(url);
   console.log(`Connected to MongoDB: ${DATABASE_NAME}`);
 }
 
